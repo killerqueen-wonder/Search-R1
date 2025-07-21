cc42337f5fd836e926ecdf3681fae85a7fe3f4c8
NEVVY <nevvy@qq.com>
2025-07-22 02:48:53 +0800

    1

--- train_ppo.sh
+++ train_ppo.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export CUDA_VISIBLE_DEVICES=0,1,2,4
+# export CUDA_VISIBLE_DEVICES=0,1,2,4
 export DATA_DIR='data/nq_search'
 
 WAND_PROJECT='Search-R1'
@@ -74,7 +74,7 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
     +trainer.val_only=false \
     +trainer.val_before_train=true \
     trainer.default_hdfs_dir=null \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=100 \
     trainer.test_freq=50 \
