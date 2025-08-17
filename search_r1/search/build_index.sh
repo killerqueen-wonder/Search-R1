98104381e9f71318055cf58d9cd61a43efedd5ac
NEVVY <nevvy@qq.com>
2025-08-17 21:25:41 +0800

    1

--- search_r1/search/build_index.sh
+++ search_r1/search/build_index.sh
@@ -1,13 +1,13 @@
 
 corpus_file=/your/corpus/jsonl/file # jsonl
-corpus_file=/your/corpus/jsonl/file # jsonl
-save_dir=/the/path/to/save/index
+corpus_file="/linzhihang/huaiwenpang/legal_LLM/dataset/law_processed.jsonl" # jsonl
+save_dir=/linzhihang/huaiwenpang/legal_LLM/dataset
 retriever_name=e5 # this is for indexing naming
 retriever_model=intfloat/e5-base-v2
 
 # change faiss_type to HNSW32/64/128 for ANN indexing
 # change retriever_name to bm25 for BM25 indexing
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python index_builder.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python index_builder.py \
     --retrieval_method $retriever_name \
     --model_path $retriever_model \
     --corpus_path $corpus_file \
